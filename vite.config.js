fa2ffe48ec2fd4b21e2d6c1e7a3a85d5c83683fa
Bobbianss <Bobbianss@users.noreply.github.com>
2023-06-29 15:32:43 +0200

    new path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: 'dist',
-    base:'/PlatformAR_RnD/',
+    base:'/PlatformAR_RnD/dist/',
     rollupOptions:{
       external: ['public/libs/*'], // specifica il file da escludere dalla build
     },
